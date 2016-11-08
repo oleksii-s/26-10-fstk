b672b041ee354c23856b9fe9b76cf47e597001da
alexDevJava28 <alexwithlenovo@gmail.com>
2016-11-08 12:01:33 +0200

    Resume, Oleksii Khodakivskyi.

---
+++ resume/index.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title>Oleksii Khodakivskyi resume</title>
+            <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+            <meta name="description" content="Full-stack developer Oleksii Khodakivskyi">
+            <meta name="keywords" content="full-stack developer, resume, Oleksii Khodakivskyi, web development,
+            front-end developer, back-end developer">
+            <meta name="author" content="Oleksii Khodakivskyi">
+
+            <link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet">
+            <script src="https://use.fontawesome.com/727dfcaab9.js"></script>
+            <link rel="stylesheet" type="text/css" href="css/style.css">
+        </head>
+
+        <body>
+            <header>
+                <h1>Oleksii Khodakivskyi</h1>
+                <h3>(06.11.1987)</h3>
+                <h2>Full-Stack Developer</h2>
+                <img src="resources/portrait.jpg" alt="Photo">
+            </header>
+
+            <nav>
+                <a href="#summary">Summary</a> |
+                <a href="#highlights">Highlights</a> |
+                <a href="#accomplishments">Accomplishments</a> |
+                <a href="#experience">Experience</a>
+                <a href="#education">Education</a> |
+                <a href="#certifications">Certifications</a> |
+                <a href="#languages">Languages</a> |
+                <a href="#personal_qualities">Personal qualities</a> |
+                <a href="#interests">Interests</a>
+            </nav>
+
+            <main role="main">
+                <section>
+                    <h3><a name="summary">Summary</a></h3>
+                    <p>
+                        An energetic and imaginative young web developer with a background in JavaScript, HTML, CSS,
+                        Java Core and SQL. I have an intense desire to learn how to improve the webs core functionality,
+                        and to also be involved in its future development. I have a good understanding of programming,
+                        can come up with plenty of innovative ideas and possesses communication skills. Right now I am
+                        looking for an entry level position with an exciting company that wants to attract talented
+                        people.
+                    </p>
+                </section>
+
+                <section>
+                    <h3><a name="highlights">Highlights</a></h3>
+                    <ul>
+                        <li>Html, CSS, JavaScript, PHP;</li>
+                        <li>Bootstrap, JQuery, React;</li>
+                        <li>SQL;</li>
+                        <li>Team player;</li>
+                        <li>Customer-oriented.</li>
+                    </ul>
+                </section>
+
+                <section>
+                    <h3><a name="accomplishments">Accomplishments</a></h3>
+                    <p>
+                        Designed and developed my own page, that describes me as a specialist. It include some
+                        information about me, certificates that I take, my portfolio and contacts.
+                        (<a href="http://www.oleksiikhodakivskyi.com/" target="_blank">http://www.oleksiikhodakivskyi.com/</a>)
+                    </p>
+                    <p>
+                        I made the page about employees of BeUnique company. Site was written on Yii Framework, so I
+                        learned a structure of this framework and wrote controller and model for this page and views for
+                        front end page and admin page, where the client can add new employee, or update current employee,
+                        or delete employee. I added new table of employees to the MySql database. When I wrote this page
+                        I used Html, CSS(LESS), JavaScript with JQuery(Ajax) and PHP.
+                    </p>
+                    <p>
+                        I have some mini study projects on CodePen.io -
+                        <a href="http://codepen.io/alexDevJava28/" target="_blank">http://codepen.io/alexDevJava28/</a>.
+                    </p>
+                </section>
+
+                <section>
+                    <h3><a name="experience">Experience</a></h3>
+                    <p>
+                        <h4>Deputy Finance Director</h4>
+                        <p>November 2013 to Current</p>
+                        <p>LLC Sport and recreation club “Monitor” – Kyiv, Ukraine</p>
+                        <ul>
+                            <li>make registry of payments for every day;</li>
+                            <li>make control clients and staff indebtedness to the company;</li>
+                            <li>make control that all departments make financial plan.</li>
+                        </ul>
+                    </p>
+                </section>
+
+                <section>
+                    <h3><a name="education">Education</a></h3>
+                    <p>Kyiv National Economic University named after Vadym Getman, 2013</p>
+                    <p>Master of Science</p>
+                    <p>Economics and Management Faculty</p>
+                </section>
+
+                <section>
+                    <h3><a name="certifications">Certifications</a></h3>
+                    <ul>
+                        <li>Free Code Camp, 2016, Front End Development Certificate;</li>
+                        <li>Lviv IT School, 2016, Web UI Certificate;</li>
+                        <li>
+                            Computer Science Center, 2015, Java SE. Basic Course
+                            <a href="https://stepic.org/certificate/0326dee60053ec7f73361430dc2419a32545d03b.pdf" target="_blank">
+                                https://stepic.org/certificate/0326dee60053ec7f73361430dc2419a32545d03b.pdf
+                            </a>;
+                        </li>
+                        <li>Institute of computer and information science, 2015, Java SE;</li>
+                    </ul>
+                </section>
+
+                <section>
+                    <h3><a name="languages">Languages</a></h3>
+                    <p>Ukrainian - native;</p>
+                    <p>Russian - native;</p>
+                    <p>English - upper-intermediate.</p>
+                </section>
+
+                <section>
+                    <h3><a name="personal_qualities">Personal qualities</a></h3>
+                    <p>Hardworking, quick study, productive, analytical, high-energy, attentive, communication.</p>
+                </section>
+
+                <section>
+                    <h3><a name="interests">Interests</a></h3>
+                    <p>Programming, snowboarding, boxing, guitar.</p>
+                </section>
+            </main>
+
+            <button id="scrollMe" type="button"><i class="fa fa-chevron-up fa-2x" aria-hidden="true"></i></button>
+
+            <address>
+                <section>
+                    <a href="https://www.facebook.com/profile.php?id=100004378097372" target="_blank">
+                        <i class="fa fa-facebook-square fa-3x" aria-hidden="true"></i>
+                    </a>
+                    <a href="https://ua.linkedin.com/in/oleksii-khodakivskyi-5727a910a" target="_blank">
+                        <i class="fa fa-linkedin-square fa-3x" aria-hidden="true"></i>
+                    </a>
+                    <a href="https://plus.google.com/u/0/101613707886693752979" target="_blank">
+                        <i class="fa fa-google-plus-square fa-3x" aria-hidden="true"></i>
+                    </a>
+                </section>
+                <section>
+                    <p>Email: alexwithlenovo@gmail.com</p>
+                    <p>Tel: +38 (098) 485-11-42</p>
+                 </section>
+            </address>
+
+            <script src="js/backToTop.js"></script>
+        </body>
+    </html>
