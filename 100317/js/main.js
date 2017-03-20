989c69e649c65ceec8e8495f754e14d7b1e33e27
alexDevJava28 <alexwithlenovo@gmail.com>
2017-03-21 00:49:20 +0200

    210317

--- 100317/js/main.js
+++ 100317/js/main.js
@@ -4,6 +4,13 @@ var root = "http://jsonplaceholder.typicode.com";
 var xhr = new XMLHttpRequest();
 var main_div = document.getElementById("main");
 
+window.onpopstate = function(e){
+    if(e.state){
+        document.getElementById("main").innerHTML = e.state.html;
+        document.title = e.state.pageTitle;
+    }
+};
+
 document.addEventListener("DOMContentLoaded", function () {
     var data = getData(xhr, root, "/posts", "");
     var users = getData(xhr, root, "/users", "");
@@ -15,15 +22,36 @@ document.addEventListener("DOMContentLoaded", function () {
 
     var links = document.getElementsByTagName("a");
 
-    for (var j = 0; j < links.length; j++) {
-        links[j].addEventListener("click", function (event) {
-            event.preventDefault();
-            alert(links[i].href);
-        })
+    for (var k = 0; k < links.length; k++) {
+        links[k].addEventListener("click", function () {
+            while (main_div.firstChild) {
+                main_div.removeChild(main_div.firstChild);
+            }
+
+            var parsed_url = this.href.split("/");
+            console.log(parsed_url);
+
+            history.pushState({}, null, this.href);
+
+            return false;
+        });
     }
+
+    history.pushState({}, null, "./posts");
 });
 
+document.getElementById("users").onclick = function (e) {
+    var users = getData(xhr, root, "/users", "");
 
+    while (main_div.firstChild) {
+        main_div.removeChild(main_div.firstChild);
+    }
+
+    main_div.appendChild(createAuthorsList(users));
+
+    history.pushState({}, null, "./users");
+    return false;
+};
 
 function getData(xhr, root, type, id) {
     if ("" != id) {
@@ -78,4 +106,22 @@ function createArticle(post, users) {
     article.appendChild(article_body);
 
     return article;
+}
+
+function createAuthorsList(users) {
+    var ul = document.createElement("ul");
+
+    for (var i = 0; i < users.length; i++) {
+        var li = document.createElement("li");
+        var a = document.createElement("a");
+
+        a.innerText = users[i].name;
+        a.className = "author";
+        a.setAttribute("id", users[i].id);
+        a.setAttribute("href", "./users/" + users[i].id);
+        li.appendChild(a);
+        ul.appendChild(li);
+    }
+
+    return ul;
 }
