13be7a14da0dfe62f4d61d37097a0638dd4763f0
alexDevJava28 <alexwithlenovo@gmail.com>
2017-03-20 17:41:26 +0200

    Еще не готово2

--- 100317/js/main.js
+++ 100317/js/main.js
@@ -2,29 +2,28 @@
 
 var root = "http://jsonplaceholder.typicode.com";
 var xhr = new XMLHttpRequest();
-var posts_links = [];
-var users_links = [];
+var main_div = document.getElementById("main");
 
 document.addEventListener("DOMContentLoaded", function () {
     var data = getData(xhr, root, "/posts", "");
     var users = getData(xhr, root, "/users", "");
 
-    var main_div = document.getElementById("main");
-
     for (var i = 0; i <data.length; i++) {
-        var article = createArticle(data[i]);
+        var article = createArticle(data[i], users);
         main_div.appendChild(article);
     }
 
-    posts_links = document.getElementsByClassName("title");
-    users_links = document.getElementsByClassName("author");
+    var links = document.getElementsByTagName("a");
+
+    for (var j = 0; j < links.length; j++) {
+        links[j].addEventListener("click", function (event) {
+            event.preventDefault();
+            alert(links[i].href);
+        })
+    }
 });
 
-posts_links.forEach(function () {
-    this.addEventListener("click", function () {
-        var post = getData(xhr, root, "/posts", this.id)
-    })
-});
+
 
 function getData(xhr, root, type, id) {
     if ("" != id) {
@@ -45,7 +44,7 @@ function getData(xhr, root, type, id) {
     return data;
 }
 
-function createArticle(post) {
+function createArticle(post, users) {
     var article = document.createElement("article");
     var link_header = document.createElement("a");
     var article_header = document.createElement("h2");
@@ -57,6 +56,7 @@ function createArticle(post) {
     link_header.innerText = post.title;
     link_header.className = "title";
     link_header.setAttribute("id", post.id);
+    link_header.setAttribute("href", "./posts/" + post.id);
     article_header.appendChild(link_header);
 
     //author element
@@ -66,6 +66,7 @@ function createArticle(post) {
     link_author.innerText = author[0].name;
     link_author.className = "author";
     link_author.setAttribute("id", author[0].id);
+    link_author.setAttribute("href", "./users/" + author[0].id);
     article_author.appendChild(link_author);
 
     //body element
