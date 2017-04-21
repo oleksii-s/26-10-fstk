be80a8d5b045c2a8532569911566dea37024b70f
Lashchenkova <Slashchenkova@gmail.com>
2017-04-21 22:55:57 +0300

    added sidebar and localStorage

--- news_portal/js/index.js
+++ news_portal/js/index.js
@@ -18,7 +18,7 @@ let request1 = dataProvider('GET', target)
             let {userId, id, body, title} = data[i];
             let cell = document.getElementsByClassName('col-xs-12 col-sm-4')[i];
             if (i < data.length) {
-                let author_link = "<a href='author_articles.shtml?id=" + userId + "'></a>";
+                let author_link = "<a class='author_link' href='author_articles.shtml?id=" + userId + "'></a>";
                 let title_link = '<a href="article.shtml?id=' + id + '">' + title + '</a>';
                 let author = "<span class='author'>" + author_link + "</span><br>";
                 let title1 = '<span class="title">' + title_link + '</span><br><br>';
@@ -37,11 +37,20 @@ let request1 = dataProvider('GET', target)
 let request2 = dataProvider('GET', author_target)
     .then(response => JSON.parse(response))
     .then(data => {
-        let class_author = document.getElementsByClassName('author');
+        let aside = document.getElementsByTagName('aside')[0].getElementsByTagName('ul')[0];
+        for (let i = 0; i < data.length; i++) {
+            let author = "'" + data[i].name + "'";
+            aside.innerHTML += '<li><a href="javascript:void(0)" onclick="chooseAuthor(' + author + ')">' + data[i].name + '</a></li>';
+        }
+        aside.innerHTML += '<li><b><a href="javascript:void(0)" onclick="chooseAuthor(123)">Show all</a></b></li>';
+        return data;
+    })
+    .then(data => {
+        let class_author = document.getElementsByClassName('author_link');
         for (let j = 0; j < class_author.length; j++) {
-            let userId = class_author[j].getElementsByTagName('a')[0].getAttribute('href');
+            let userId = class_author[j].getAttribute('href');
             userId = userId.replace('author_articles.shtml?id=', '') - 1;
-            class_author[j].getElementsByTagName('a')[0].innerHTML = data[userId].name;
+            class_author[j].innerHTML = data[userId].name;
         }
     })
     .catch(error => {
@@ -49,7 +58,10 @@ let request2 = dataProvider('GET', author_target)
     });
 
 Promise.all([request1, request2])
-    .then(results => {})
+    .then(results => {
+        let storage = localStorage.getItem("author");
+        chooseAuthor(storage);
+    })
     .catch(error => {
         alert(error);
     });
@@ -73,4 +85,60 @@ function dataProvider(method, url) {
 
         xhr.send();
     });
+}
+//show or hide sidebar
+function showHide() {
+    let obj = document.getElementById('slideout');
+    let wrapper = document.getElementById('wrapper');
+    let button = obj.getElementsByClassName('glyphicon')[0];
+
+    if(obj.style.left != "-13%") {
+        obj.style.left = '-13%';
+        wrapper.style.width = "90%";
+        wrapper.style.float = "none";
+        wrapper.style.marginRight = "auto";
+        button.className = 'glyphicon glyphicon-menu-right';
+    } else {
+        obj.style.left = "0";
+        wrapper.style.width = "80%";
+        wrapper.style.float = "right";
+        wrapper.style.marginRight = "30px";
+        button.className = 'glyphicon glyphicon-menu-left';
+    }
+}
+
+function chooseAuthor(author) {
+    let row = document.getElementsByClassName("row");
+
+    for (let j = 0; j < row.length; j++) {
+        let div = row[j].getElementsByClassName('col-xs-12 col-sm-4');
+
+        for (let i = 0; i < div.length; i++) {
+            let link = div[i].getElementsByClassName('author_link')[0];
+
+            if (link == undefined || link.innerHTML != author) {
+                div[i].style.display = 'none';
+                localStorage['author'] = author;
+            } else {
+                div[i].style.display = '';
+                localStorage['author'] = '';
+            }
+
+            if (author == 123) {
+                row[j].style.display = '';
+                localStorage['author'] = "";
+                for (let b = 0; b < div.length; b++) {
+                    div[b].style.display = '';
+                }
+            }
+        }
+
+        if (div[0].style.display == 'none' &&
+            div[1].style.display == 'none' &&
+            div[2].style.display == 'none') {
+            row[j].style.display = 'none';
+        } else {
+            row[j].style.display = '';
+        }
+    }
 }
