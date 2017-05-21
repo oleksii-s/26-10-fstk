14bd17aaed3b60bec941d44bb32ca0c4cbba285f
Lashchenkova <Slashchenkova@gmail.com>
2017-05-21 23:53:21 +0300

    deleted files

--- startsWith.js
+++
@@ -1,9 +0,0 @@
-function startsWith(str1, str2) {
-    return !!~str2.toLowerCase().indexOf(str1[0].toLowerCase());
-    }
-}
-//return Boolean(~str2.toLowerCase().indexOf(str1[0].toLowerCase()));
-
-// var a = 'History of the Civil War',
-//     b = 'april';
-// startsWith(a, b);
